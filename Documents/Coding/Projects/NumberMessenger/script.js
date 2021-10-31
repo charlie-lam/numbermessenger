a88b92c4f36bd808e97a86e4268386a05deac318
charlie.lamb <char.lam@ntlworld.com>
2021-10-31 16:33:53 +0000

    Full messenger coding

--- Documents/Coding/Projects/NumberMessenger/script.js
+++ Documents/Coding/Projects/NumberMessenger/script.js
@@ -1,9 +1,11 @@
-const messenger = () => {
+const numbMessenger = () => {
     const num1arr = [0,1,2,3,4,5]
     const num2arr = [0,1,2,3,4,5]
     const num3arr = [0,1,2,3,4,5]
-    let num1 = num1arr[Math.floor(Math.random())*num1arr.length - 1]
-    console.log(num1)
+    let num1 = num1arr[Math.floor(Math.random()*num1arr.length)]
+    let num2 = num2arr[Math.floor(Math.random()*num2arr.length)]
+    let num3 = num3arr[Math.floor(Math.random()*num3arr.length)]
+    console.log(`Random ${num1}, next up ${num2} and finally ${num3}.`)
 }
 
-messenger();
+numbMessenger();

4b4ae4022e7cd380556be3cc227691b7887fe317
charlie.lamb <char.lam@ntlworld.com>
2021-10-31 16:04:37 +0000

    First project commit

---
+++ Documents/Coding/Projects/NumberMessenger/script.js
@@ -0,0 +1,9 @@
+const messenger = () => {
+    const num1arr = [0,1,2,3,4,5]
+    const num2arr = [0,1,2,3,4,5]
+    const num3arr = [0,1,2,3,4,5]
+    let num1 = num1arr[Math.floor(Math.random())*num1arr.length - 1]
+    console.log(num1)
+}
+
+messenger();
